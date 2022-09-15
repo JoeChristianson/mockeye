1d7332c3dfdb3c5130b7c6420fc6820a21d65a6a
Joseph Christianson <joechristiansonwebdev@gmail.com>
2022-09-15 17:07:43 -0500

    Issue #16 - added type names as exports

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const fake = require("./make/index.js");
 const write = require("./write/index.js")
+let types = require("./types")
+let typesByName = {}
+for(let name in types){
+    typesByName[name]=name
+}
+types = typesByName
 
-
-module.exports = {fake,write}
+module.exports = {fake,write,types}
