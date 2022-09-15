1fabb2e7da580da8332b56ea204235c2bba2b78d
Joseph Christianson <joechristiansonwebdev@gmail.com>
2022-09-14 21:45:40 -0500

    Issue #3 - created both array and object methods, can do nested objects now too

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello world!")
+import make from "./make/index.js"
+import util from "util"
+
+const obj = [{window:[{day:"day"},5]},3]
+
+console.log(util.inspect(make(obj),{showHidden:false,depth:null}));
+
+console.log(JSON.stringify(make(obj)))
+
---
+++ utils/random.js
@@ -0,0 +1,3 @@
+export const randomElementFromArray = (array)=>{
+    return array[Math.floor(Math.random()*array.length)]
+}
