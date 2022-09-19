efec73b9e9ea9537156506414ece38ae8453976f
Joseph Christianson <joechristiansonwebdev@gmail.com>
2022-09-19 10:27:20 -0500

    Issue 19 - added some web types

--- types/web.js
+++ types/web.js
@@ -1,11 +1,22 @@
 const separatedNumbers = require("../utils/separatedNumbers")
-
+const fs = require("fs")
 const types = {
     url:()=>{
         return ["http://www.google.com"]
     },
     ipAddress:()=>{
         return [separatedNumbers([[0,255],[0,255],[0,255],[0,255]],".")]
+    },
+    imageUrl:()=>{
+        try{
+
+            const imageList = JSON.parse(fs.readFileSync("./jsonResources/testImages.json").toString());
+            return imageList
+            
+        }catch(err){
+            console.error(err)
+        }
+        
     }
 }
 
